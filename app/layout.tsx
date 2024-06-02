8e566563bb96b4774abd5a97fb70b06dc6cf8858
DarkInventor <ktmehta25@gmail.com>
2024-06-02 18:56:17 -0400

    add vercel --speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { MainNav } from "@/components/main-nav";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 import { fontSans } from "@/lib/fonts";
@@ -70,6 +71,7 @@ export default function RootLayout({
 
           <main className="flex-1">{children}</main>
           <Analytics />
+          <SpeedInsights />
           {/* </div>
            
           <SiteFooter /> */}
