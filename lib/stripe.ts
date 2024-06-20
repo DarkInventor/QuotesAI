7a4f9a7925db20ec26af0e8f398a741af8799e4a
DarkInventor <ktmehta25@gmail.com>
2024-06-20 10:36:05 -0400

    fix stripe --version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe"
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY || '', {
-  apiVersion: "2023-10-16",
+  apiVersion: "2023-08-16", // Adjusted to the correct available version as per the followup instructions
   typescript: true,
 })
