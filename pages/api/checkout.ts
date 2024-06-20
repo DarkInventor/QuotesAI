584256f1632b42c74ebc21efbcd40d77ae15e6f5
DarkInventor <ktmehta25@gmail.com>
2024-06-20 10:41:42 -0400

    fix stripe --version

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -6,7 +6,7 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth/next";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2023-10-16",
+  apiVersion: "2023-08-16",
 });
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET || "";
